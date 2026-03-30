37fd909db75961066931ac9c0fff1fc25aa60215
amarreddypannala <amarreddypannala@gmail.com>
2026-03-30 14:58:59 +0530

    fix backend s3 and DynamoDb config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "amar-devops-dev-1"
+    bucket         = "amar-devops-tfstate-2026-001"
     key            = "devops-project/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
