42e1cb3490ae2bde6b10f371934eb413a1e3a091
apannala_private <85744501+amarreddypannala@users.noreply.github.com>
2026-04-01 18:57:15 +0530

    Setup k8 (#40)
    * bootstrap script modified to compatible Amazon Linux 2023
    
    * bootstrap script modified

--- terraform/global/variables.tf
+++ terraform/global/variables.tf
@@ -1,3 +1,12 @@
 variable "aws_region" {
   type = string
 }
+variable "dev_instance_id" {
+  description = "EC2 instance ID for dev"
+  type        = string
+}
+
+variable "prod_instance_id" {
+  description = "EC2 instance ID for prod"
+  type        = string
+}
