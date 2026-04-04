5a3e5cddad36eda60169ff505fdfcc46c375f78c
apannala_private <85744501+amarreddypannala@users.noreply.github.com>
2026-04-04 14:26:16 +0530

    Fix wf02 (#52)
    * fixing k8s deployment to not destroy unless specified
    
    * fixed main.tf for devk8s

--- terraform/k8s-dev/main.tf
+++ terraform/k8s-dev/main.tf
@@ -19,6 +19,15 @@ resource "kubernetes_deployment" "nginx" {
       }
     }
 
+    strategy {
+      type = "RollingUpdate"
+
+      rolling_update {
+        max_unavailable = 1
+        max_surge       = 1
+      }
+    }
+
     template {
       metadata {
         labels = {
@@ -28,7 +37,7 @@ resource "kubernetes_deployment" "nginx" {
 
       spec {
         container {
-          image = "nginx:latest"
+          image = "nginx:1.29.7"
           name  = "nginx"
 
           port {
