64e65a9a3a68df1100335ea6efdad6e707855a4a
apannala_private <85744501+amarreddypannala@users.noreply.github.com>
2026-04-10 14:51:40 +0530

    fix the file storage error for loki and add timeout to grafana (#66)

--- terraform/o11y-prod/loki.tf
+++ terraform/o11y-prod/loki.tf
@@ -1,9 +1,3 @@
-resource "kubernetes_namespace_v1" "loki" {
-  metadata {
-    name = var.loki_namespace
-  }
-}
-
 resource "helm_release" "loki" {
   name      = "loki"
   namespace = kubernetes_namespace_v1.loki.metadata[0].name
@@ -11,17 +5,49 @@ resource "helm_release" "loki" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "loki"
 
+  timeout = 600
+
   values = [
     <<EOF
+deploymentMode: SingleBinary
+
 loki:
   auth_enabled: false
+  commonConfig:
+    replication_factor: 1
+  storage:
+    type: filesystem
+  schemaConfig:
+    configs:
+      - from: "2024-04-01"
+        store: tsdb
+        object_store: filesystem
+        schema: v13
+        index:
+          prefix: loki_index_
+          period: 24h
 
 singleBinary:
   replicas: 1
+  persistence:
+    enabled: true
+    size: 10Gi
 
-persistence:
-  enabled: true
-  size: 10Gi
+backend:
+  replicas: 0
+read:
+  replicas: 0
+write:
+  replicas: 0
+
+chunksCache:
+  enabled: false
+
+resultsCache:
+  enabled: false
+
+minio:
+  enabled: false
 EOF
   ]
 }
