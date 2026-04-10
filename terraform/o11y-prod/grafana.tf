f184616137403eb519f2e203c632184d15ea7c28
amarreddypannala <amarreddypannala@gmail.com>
2026-04-10 14:51:01 +0530

    fix the file storage error for loki and add timeout to grafana

--- terraform/o11y-prod/grafana.tf
+++ terraform/o11y-prod/grafana.tf
@@ -1,15 +1,11 @@
-resource "kubernetes_namespace_v1" "grafana" {
-  metadata {
-    name = var.grafana_namespace
-  }
-}
-
 resource "helm_release" "grafana" {
   name      = "grafana"
   namespace = kubernetes_namespace_v1.grafana.metadata[0].name
 
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
+
+  timeout = 600
 
   values = [
     <<EOF
